64519cbcd2a34cdbcbc35422869017fe97627873
Evgeny Grishanin <evgrishanin@gmail.com>
2016-03-07 18:25:38 +0300

    Fix table markup

--- js/components/GameTable.js
+++ js/components/GameTable.js
@@ -17,6 +17,7 @@ const RollsResult = (rolls) => {
 
 const GameTable = ({ players, game: { frames, points } }) => {
   const framesCount = 10;
+  const tbodyRows = [];
   const tableContent = [
     h('DIV', {className: 'thead'}, [
       h('DIV', {className: 'row'}, [
@@ -24,48 +25,42 @@ const GameTable = ({ players, game: { frames, points } }) => {
         ...Array.apply(null, {length: framesCount}).map((v, i ) => h('DIV', {className: 'cell'}, [i + 1])),
         h('DIV', {className: 'cell cell_total-points'}, ['Points'])
       ]
-    )
-  ])
-];
-
-const table = h('DIV', {className: 'table'}, tableContent);
-
-Object.keys(players).forEach((player, playerIndex) => {
-  const tbodyCells = [
-    h('DIV', {className: 'cell'}, [player])
+    )]),
+    h('DIV', {className: 'tbody'}, tbodyRows)
   ];
 
-  tableContent.push(...[
-    h('DIV', {className: 'tbody'}, [
-      h('DIV', {className: 'row'}, tbodyCells)
-    ])
-  ]);
+  const table = h('DIV', {className: 'table'}, tableContent);
 
-  for (let i = 0; i < framesCount; i++) {
-    let frameRolls = '';
-    let framePoints = '';
-    const playerFrames = frames[playerIndex];
+  Object.keys(players).forEach((player, playerIndex) => {
+    const rowCells = [
+      h('DIV', {className: 'cell'}, [player])
+    ];
+    tbodyRows.push(h('DIV', {className: 'row'}, rowCells));
 
-    if (playerFrames) {
-      framePoints = points[playerIndex][i];
-      frameRolls = playerFrames[i] ? h('DIV', [
-        h('DIV', [RollsResult(playerFrames[i])]),
-        h('DIV', [framePoints])
-      ]) : '';
+    for (let i = 0; i < framesCount; i++) {
+      let frameRolls = '';
+      let framePoints = '';
+      const playerFrames = frames[playerIndex];
+      if (playerFrames) {
+        framePoints = points[playerIndex][i];
+        frameRolls = playerFrames[i] ? h('DIV', [
+          h('DIV', [RollsResult(playerFrames[i])]),
+          h('DIV', [framePoints])
+        ]) : '';
+      }
+
+      rowCells.push(
+        h('DIV', {className: 'cell'}, [frameRolls])
+      );
     }
 
-    tbodyCells.push(
-      h('DIV', {className: 'cell'}, [frameRolls])
+    const totalPoints = points[playerIndex] ? points[playerIndex].reduce((a, b) => a + b, 0) : 0;
+    rowCells.push(
+      h('DIV', {className: 'cell'}, [totalPoints])
     );
-  }
+  });
 
-  const totalPoints = points[playerIndex] ? points[playerIndex].reduce((a, b) => a + b, 0) : 0;
-  tbodyCells.push(
-    h('DIV', {className: 'cell'}, [totalPoints])
-  );
-})
-
-return table;
+  return table;
 }
 
 export default GameTable;
